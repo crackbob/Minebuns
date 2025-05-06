b8809415fa1e1fa1088bd7dad6167c115665adce
crackbob <crackbob.999@gmail.com>
2025-05-06 17:14:09 -0400

    userscript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,48 @@
 const path = require("path");
+const { UserscriptPlugin } = require("webpack-userscript");
 
-module.exports = {
+const commonConfig = {
     mode: "production",
     entry: "./src/main.js",
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "Minebuns.min.js",
-    },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ["style-loader", "css-loader"]
             }
         ]
     }
 };
+
+const userscriptConfig = {
+    ...commonConfig,
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "Minebuns.user.js",
+    },
+    plugins: [
+        new UserscriptPlugin({
+            metajs: false,
+            headers: {
+                name: "Minebuns",
+                supportURL: "https://github.com/crackbob/Minebuns",
+                description: "we do a little trolling",
+                version: "1.0.0",
+                author: "crackbob",
+                match: "*://minefun.io/*",
+                grant: "none",
+            },
+        }),
+    ],
+};
+
+const regularScriptConfig = {
+    ...commonConfig,
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "Minebuns.min.js",
+    },
+    plugins: [],
+};
+
+module.exports = [userscriptConfig, regularScriptConfig];
